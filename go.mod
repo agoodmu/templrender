e567874b62e1843f4d4c9c5dcd50b22786ace57b
root <root@M84301795-FEiuy.china.huawei.com>
2025-06-25 15:19:35 +0800

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/agoodmu/templrender
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,27 @@
+package templrender
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/a-h/templ"
+)
+
+type TemplRender struct {
+	Context   context.Context
+	Component templ.Component
+}
+
+func (t TemplRender) Render(w http.ResponseWriter) error {
+	t.WriteContentType(w)
+	if t.Component == nil {
+		return errors.New("invalid templ componet")
+	}
+	return t.Component.Render(t.Context, w)
+
+}
+
+func (t TemplRender) WriteContentType(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+}
